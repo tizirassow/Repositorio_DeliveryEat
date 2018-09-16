c4e6196369a8355fa3e7ba2e573fefa4b26dde3b
Juanmalopezg <juanmaa79@gmail.com>
2018-09-16 17:42:46 -0300

    Formulario nuevo

--- Producto/02 - Código Fuente/03 - Web/DeliveryEat/src/app/app.module.ts
+++ Producto/02 - Código Fuente/03 - Web/DeliveryEat/src/app/app.module.ts
@@ -1,16 +1,53 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+// import {AppRoutingModule} from './app-routing.module';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {MaterialModule} from './material/material.module';
+import { AgmCoreModule } from '@agm/core';
+import { NgXCreditCardsModule } from 'ngx-credit-cards';
+
+
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {LayoutModule} from '@angular/cdk/layout';
+
+import {ComerciosService} from './shared/comercios.service';
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
+import {ComercioComponent} from './comercios/comercio.component';
+import {PagosComponent} from './pagos/pagos.component';
+import {PedidoComponent} from './pedido/pedido.component';
+import {ProductoComponent} from './productos/producto.component';
+import { DetallePedidoComponent } from './pedido/detalle-pedido/detalle-pedido.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComercioComponent,
+    PagosComponent,
+    PedidoComponent,
+    ProductoComponent,
+    DetallePedidoComponent
+
   ],
   imports: [
-    BrowserModule
+    // AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    LayoutModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    NgXCreditCardsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'YOUR_KEY',
+      libraries: ['places']
+    })
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ComerciosService],
+  bootstrap: [AppComponent],
+  entryComponents: [PedidoComponent]
+
 })
-export class AppModule { }
+export class AppModule {
+}
