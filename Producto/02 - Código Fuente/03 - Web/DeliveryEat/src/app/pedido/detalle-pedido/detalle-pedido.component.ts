d5aa260019830f04090aee2a9a7b039a32399209
Juanmalopezg <juanmaa79@gmail.com>
2018-10-23 00:41:28 -0300

    Correcciones en validadores. Final para exportar

--- Producto/02 - Código Fuente/03 - Web/DeliveryEat/src/app/pedido/detalle-pedido/detalle-pedido.component.ts
+++ Producto/02 - Código Fuente/03 - Web/DeliveryEat/src/app/pedido/detalle-pedido/detalle-pedido.component.ts
@@ -1,8 +1,7 @@
-import {Component,Inject} from '@angular/core';
+import {ChangeDetectorRef, Component} from '@angular/core';
 import {Time} from '@angular/common';
 import {FormBuilder, FormGroup, Validators, NgForm} from '@angular/forms';
 import {MatSnackBar} from '@angular/material';
-
 import {CreditCardValidator} from 'ngx-credit-cards';
 
 
@@ -22,27 +21,31 @@ export class DetallePedidoComponent {
   today =  new Date();
   fechaEntrega: Date = null;
   horaEntrega: Time = null;
+  esYa: number = 0;
   formaPago: '';
   numeroTarjeta: '';
   nombreTarjeta: '';
   fechaExp: ' ';
   codigoSeg: '';
   pagoCon: '';
-  esYa: number = 0;
 
+  //Declaro array de pedido hardcodeado
   displayedColumns: string[] = ['item', 'cost'];
   pedidoHard: Pedido[] = [{
     item: 'Kilo de helado', cost: 250, notas: 'Sabores: Ferrero Rocher, Tramontana'
   }, {item: 'Bombón escocés - 6u', cost: 150, notas: ''}];
 
-  constructor(private fb: FormBuilder, public snackBar: MatSnackBar) {
+  constructor(private fb: FormBuilder, public snackBar: MatSnackBar,private cd: ChangeDetectorRef) {
 
     // Incializo los formularios con sus respectivas validaciones iniciales
     this.regiForm = fb.group({
-      'calle': ['', Validators.compose([Validators.required, Validators.maxLength(250)])],
-      'numeracion': ['', Validators.compose([Validators.required, Validators.minLength(1), Validators.maxLength(10),Validators.pattern('^[0-9_-]*')])],
-      'piso': ['', Validators.compose([Validators.minLength(1), Validators.maxLength(4),Validators.pattern('^[0-9_-]*')])],
-      'dpto': ['', Validators.compose([Validators.minLength(1), Validators.maxLength(2),Validators.pattern('^[a-zA-Z_-]*')])],
+      'calle': ['', Validators.compose([Validators.required, Validators.maxLength(250),Validators.pattern('^\\D+\\s(\\D+\\s+)*\\d+$')])],
+      // 'numeracion': ['', Validators.compose([Validators.required, Validators.minLength(1), Validators.maxLength(10),Validators.pattern('^[0-9_-]*')])],
+      'pisoDpto': ['', Validators.compose([Validators.minLength(1), Validators.maxLength(10)])],
+      'fechaHoraForm':fb.group({
+        'fechaEntrega': ['', Validators.compose([Validators.required, Validators.nullValidator])],
+        'horaEntrega': ['', Validators.compose([Validators.required, Validators.nullValidator])]
+      }),
       'esYa': [null]
     });
     this.pagosForm = fb.group({
@@ -51,14 +54,14 @@ export class DetallePedidoComponent {
       'cardForm': fb.group({
         'numeroTarjeta': [null],
         'nombreTarjeta': [null],
-        'fechaExp': [null],
+        'fechaExp': [''],
         'codigoSeg': [null]})
     });
     this.confirmationForm = fb.group({});
-    this.fechaHoraForm = fb.group({
-      'fechaEntrega': [null, Validators.compose([Validators.required, Validators.nullValidator])],
-      'horaEntrega': [null, Validators.compose([Validators.required, Validators.nullValidator])]
-    });
+  }
+
+  ngAfterContentInit() {
+    this.cd.detectChanges();
   }
 
   fechaHoraEspecifica(event: any) {
@@ -67,27 +70,31 @@ export class DetallePedidoComponent {
 
       //Si seleccionó "Lo antes posible", quito las validaciones de hora y fecha específica
       this.esYa = 1;
-      this.fechaHoraForm.controls['fechaEntrega'].clearValidators();
-      this.fechaHoraForm.controls['horaEntrega'].clearValidators();
-      this.fechaHoraForm.controls['fechaEntrega'].updateValueAndValidity();
-      this.fechaHoraForm.controls['horaEntrega'].updateValueAndValidity();
-      this.fechaHoraForm.updateValueAndValidity();
+      (this.regiForm.controls['fechaHoraForm'] as FormGroup).controls['fechaEntrega'].clearValidators();
+      (this.regiForm.controls['fechaHoraForm'] as FormGroup).controls['horaEntrega'].clearValidators();
+      (this.regiForm.controls['fechaHoraForm'] as FormGroup).controls['fechaEntrega'].updateValueAndValidity();
+      (this.regiForm.controls['fechaHoraForm'] as FormGroup).controls['horaEntrega'].updateValueAndValidity();
+      this.regiForm.controls['fechaHoraForm'].updateValueAndValidity();
+      this.regiForm.updateValueAndValidity();
 
     } else {
 
       //Si seleccionó "Fecha y hora específica", agrego las validaciones corespondientes
       this.esYa = 0;
-      this.fechaHoraForm.controls['fechaEntrega'].setValidators(Validators.compose([Validators.required, Validators.nullValidator]));
-      this.fechaHoraForm.controls['horaEntrega'].setValidators(Validators.compose([Validators.required, Validators.nullValidator]));
-      this.fechaHoraForm.controls['fechaEntrega'].updateValueAndValidity();
-      this.fechaHoraForm.controls['horaEntrega'].updateValueAndValidity();
-      this.fechaHoraForm.updateValueAndValidity();
+      (this.regiForm.controls['fechaHoraForm'] as FormGroup).controls['fechaEntrega'].setValidators(Validators.compose([Validators.required, Validators.nullValidator]));
+      (this.regiForm.controls['fechaHoraForm'] as FormGroup).controls['horaEntrega'].setValidators(Validators.compose([Validators.required, Validators.nullValidator]));
+      (this.regiForm.controls['fechaHoraForm'] as FormGroup).controls['fechaEntrega'].updateValueAndValidity();
+      (this.regiForm.controls['fechaHoraForm'] as FormGroup).controls['horaEntrega'].updateValueAndValidity();
+      this.regiForm.controls['fechaHoraForm'].updateValueAndValidity();
+      this.regiForm.updateValueAndValidity();
 
     }
   }
   iniciarValidadoresTarjeta(event: any) {
     if (event) {
       this.formaPago = event;
+      this.cd.detectChanges();
+
     }
     if (event === 'usaTarjeta') {
       //Si seleccionó forma de pago "Tarjeta de crédito", agrego las validaciones de tarjeta de crédito
@@ -98,24 +105,37 @@ export class DetallePedidoComponent {
       (this.pagosForm.controls['cardForm'] as FormGroup).controls['nombreTarjeta'].setValidators(Validators.compose([Validators.required, Validators.minLength(2)]));
       (this.pagosForm.controls['cardForm'] as FormGroup).controls['fechaExp'].setValidators(CreditCardValidator.validateCardExpiry);
       (this.pagosForm.controls['cardForm'] as FormGroup).controls['codigoSeg'].setValidators(CreditCardValidator.validateCardCvc);
-      this.pagosForm.controls['cardForm'].updateValueAndValidity();
 
+      (this.pagosForm.controls['cardForm'] as FormGroup).controls['numeroTarjeta'].updateValueAndValidity();
+      (this.pagosForm.controls['cardForm'] as FormGroup).controls['nombreTarjeta'].updateValueAndValidity();
+      (this.pagosForm.controls['cardForm'] as FormGroup).controls['fechaExp'].updateValueAndValidity();
+      (this.pagosForm.controls['cardForm'] as FormGroup).controls['codigoSeg'].updateValueAndValidity();
+      this.pagosForm.controls['cardForm'].updateValueAndValidity();
+      this.pagosForm.updateValueAndValidity();
+
+      this.cd.detectChanges();
     }
     else {
       //Si seleccionó forma de pago "Efectivo", agrego las validaciones correspondientes
       this.pagosForm.controls['pagoCon'].setValidators(Validators.compose([Validators.min(400),Validators.required]));
       this.pagosForm.controls['pagoCon'].updateValueAndValidity();
+      (this.pagosForm.controls['cardForm'] as FormGroup).controls['numeroTarjeta'].clearValidators();
+      (this.pagosForm.controls['cardForm'] as FormGroup).controls['nombreTarjeta'].clearValidators();
+      (this.pagosForm.controls['cardForm'] as FormGroup).controls['fechaExp'].clearValidators();
+      (this.pagosForm.controls['cardForm'] as FormGroup).controls['codigoSeg'].clearValidators();
       this.pagosForm.controls['cardForm'].clearValidators();
-      this.pagosForm.controls.cardForm = this.fb.group({
-          'numeroTarjeta': [null],
-          'nombreTarjeta': [null],
-          'fechaExp': [null],
-          'codigoSeg': [null],
-      });
+
+      (this.pagosForm.controls['cardForm'] as FormGroup).controls['numeroTarjeta'].updateValueAndValidity();
+      (this.pagosForm.controls['cardForm'] as FormGroup).controls['nombreTarjeta'].updateValueAndValidity();
+      (this.pagosForm.controls['cardForm'] as FormGroup).controls['fechaExp'].updateValueAndValidity();
+      (this.pagosForm.controls['cardForm'] as FormGroup).controls['codigoSeg'].updateValueAndValidity();
       this.pagosForm.controls['cardForm'].updateValueAndValidity();
+
       this.pagosForm.updateValueAndValidity();
+      this.cd.detectChanges();
 
     }
+
   }
 
   getTotalCost() {
@@ -144,7 +164,7 @@ export interface Pedido {
 }
 
 @Component({
-  selector: 'app-confirmacion', 
+  selector: 'app-confirmacion',
   templateUrl: '/detalle-pedido-confirmacion.html'
 })
 export class ConfirmacionComponent {
